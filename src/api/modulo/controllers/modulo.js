8170997bde74f87a428b8ef2ba62f60a557b2511
pedroduranc1 <61203515+pedroduranc1@users.noreply.github.com>
2023-07-13 16:46:23 -0400

    UPDATED DB PD 13/07/2023

---
+++ src/api/modulo/controllers/modulo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * modulo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::modulo.modulo');
